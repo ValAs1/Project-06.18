5b41962aa645fc4ea5bd8bbfc3f74ada9c918473
ValAs1 <37310121+ValAs1@users.noreply.github.com>
2018-06-26 01:03:32 +0400

    26

--- ConsoleApp2/ConsoleApp1/Program.cs
+++ ConsoleApp2/ConsoleApp1/Program.cs
@@ -10,25 +10,32 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int row = 5;
-            int col = 5;
-            int[,] array = CreatRandomArray(row, col);
-            
-            PrintMatrix(array, row, col);
-            int secMax = GetMaxSecondDiagonal(array, row, col);
-            Console.WriteLine(secMax);
+           
+            int[,] array = CreatRandomArray(5, 5);
+            PrintMatrix(array);
 
-            
+            //int max = MaxMatrix(array);
+            //Console.WriteLine(max);
+
+            //int genMax = GetMaxGeneralDiagonal(array);
+            //Console.WriteLine(genMax);
+
+            int secMax = GetMaxSecondDiagonal(array);
+            Console.WriteLine("The maximum in the secondary diagonal is - " + secMax);
+
+
+            //int lowMax = GetMaxLowerDiagonal(array);
+            //Console.WriteLine(lowMax);
             Console.ReadLine();
         }
 
-        static int[,] CreatRandomArray(int row, int col)
+        static int[,] CreatRandomArray(int count1, int count2)
         {
             Random rnd = new Random();
-            int[,] array = new int[row, col];
-            for (int i = 0; i < row; i++)
+            int[,] array = new int[count1, count2];
+            for (int i = 0; i < array.GetLength(0); i++)
             {
-                for (int j = 0; j < col; j++)
+                for (int j = 0; j < array.GetLength(1); j++)
                 {
                     array[i, j] = rnd.Next(1, 100);
                 }
@@ -36,11 +43,11 @@ namespace ConsoleApp1
             return array;
         }
 
-        static void PrintMatrix(int[,] array, int row , int col)
+        static void PrintMatrix(int[,] array)
         {
-            for (int i = 0; i < row; i++)
+            for (int i = 0; i < array.GetLength(0); i++)
             {
-                for (int j = 0; j < col; j++)
+                for (int j = 0; j < array.GetLength(1); j++)
                 {
                     Console.Write(array[i, j] + "\t");
                 }
@@ -48,21 +55,70 @@ namespace ConsoleApp1
             }
         }
 
-        //3. unenq 2 chap zangvac gtnel 2rdayin ankyunagcic amenamec@
-        static int GetMaxSecondDiagonal(int[,] array, int row, int col)
+        //1. unenq 2chap zangvac, gtnel max
+        static int MaxMatrix(int[,] array)
         {
             int max = array[0, 0];
-            for (int i = 0; i < array.Length; i++)
+            foreach (var item in array)
             {
-                for (int j = 0; j < array.Length; j++)
+                if (item > max)
                 {
-                    if (i + j == 5 - 1 && array[i, j] > max)
+                    max = item;
+                }
+            }
+            return max;
+        }
+
+        //2. unenq 2 chap zangvac gtnel glxavor ankyunagcic max@
+        static int GetMaxGeneralDiagonal(int[,] array)
+        {
+            int max = -1;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (i == j && array[i, i] > max)
+                    {
+                        max = array[i, i];
+                    }
+                }
+            }
+            return max;
+        }
+
+        //3. unenq 2 chap zangvac gtnel 2rdayin ankyunagcic amenamec@
+        static int GetMaxSecondDiagonal(int[,] array)
+        {
+            int max = array[0, array.GetLength(1)-1 ];
+            for (int i =0; i< array.GetLength(0); i++)
+            {
+                for (int j = array.GetLength(1)-1-i; j == array.GetLength(1) - i-1; j--)
+                {
+                    if (array[i, j] > max)
                     {
                         max = array[i, j];
                     }
                 }
             }
             return max;
+        }
+
+        // 4. unenq 2chap zangvac gtnel glxavor ankyunagci u dranic nerqev gtnvox elementneric max
+        static int GetMaxLowerDiagonal(int[,] array)
+        {
+            int max = array[0, 0];
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (i >= j && array[i, j] > max)
+                    {
+                        max = array[i, j];
+                    }
+                }
+            }
+            return max;
+
         }
     }
 }
---
+++ ConsoleApp2/ConsoleApp3/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = CreateRandomArray(4, 4, 0, 20);
+            PrintArray(array);
+            Console.WriteLine("The maximum of the array is " + GetMax(array));
+            Console.WriteLine("The maximum in the main diagonal is " + GetMaxinMainDiagonal(array, 4, 4));
+            Console.WriteLine("The maximum in the secondary diagonal is " + GetMaxinSecondaryDiagonal(array, 4, 4));
+            Console.WriteLine("The maximum under the main diagonal is  " + GetMaxUnderMainDiagonal(array, 4, 4));
+        }
+
+        static int[,] CreateRandomArray(int length, int width, int minValue, int maxValue)
+        {
+            int[,] arr;
+            arr = new int[length, width];
+            Random rand = new Random();
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    arr[i, j] = rand.Next(minValue, maxValue);
+                }
+            }
+            return arr;
+        }
+
+        static void PrintArray(int[,] arr)
+        {
+            foreach (int item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static int GetMax(int[,] arr)
+        {
+            int max = arr[0, 0];
+            foreach (int item in arr)
+            {
+                if (item > max)
+                    max = item;
+            }
+            return max;
+        }
+
+        static int GetMaxinMainDiagonal(int[,] arr, int length, int width)
+        {
+            int max = arr[0, 0];
+            for (int i = 0; i < length; i++)
+            {
+                if (arr[i, i] > max)
+                    max = arr[i, i];
+            }
+            return max;
+        }
+
+        static int GetMaxinSecondaryDiagonal(int[,] arr, int length, int width)
+        {
+            int max = arr[0, length - 1];
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = length - 1; j >= 0; j--)
+                {
+                    if (arr[i, j] > max)
+                        max = arr[i, j];
+                }
+            }
+            return max;
+        }
+
+        static int GetMaxUnderMainDiagonal(int[,] arr, int length, int width)
+        {
+            int max = arr[1, 0];
+            for (int i = 1; i < length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (arr[i, j] > max)
+                        max = arr[i, j];
+                }
+            }
+            return max;
+        }
+
+
+    }
+}
