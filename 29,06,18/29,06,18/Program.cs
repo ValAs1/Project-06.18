0e5c15de37bb36423c67f5f983da69e9a2eecdea
ValAs1 <37310121+ValAs1@users.noreply.github.com>
2018-06-29 15:53:40 +0400

    29.06

---
+++ 29,06,18/29,06,18/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29_06_18
+{
+    class Program
+    {
+        //  1.  unenq 2chap zangvac gtnel(edited)
+
+        //1.1 yuraqanchyur toxi mecaguyn@ (edited)
+
+        //1.2 yuraqanchyur syan mecaguyn@ (edited)
+
+        //2. unenq zangvaci zangvac(qrqrvac, jagged) (edited)
+
+        //2.1 gtnel yuraqanchyur toxi(zangvaci ) gumar@ (edited)
+
+        //2.2 gtnel yuraqancyur toxi max @ (edited)
+        
+        //kardal rev. out .
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
