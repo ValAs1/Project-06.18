d6522bc2ec8f10f74fd7f3c92f62f2ac228aeee2
ValAs1 <37310121+ValAs1@users.noreply.github.com>
2018-07-04 14:57:39 +0400

    03.07
    03.7

---
+++ Homework 28.06/ConsoleApp2/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    
+        class Program
+        {
+            /*2. unenq zangvaci zangvac (qrqrvac, jagged) 
+            2.1 gtnel yuraqanchyur toxi (zangvaci ) gumar@ 
+            2.2 gtnel yuraqancyur toxi max @*/
+            static void Main(string[] args)
+            {
+                int[][] array = CreatJagedArrays();
+
+                int[][] jarr = CreatRandomArray(array);
+
+                Print(jarr);
+
+                int[] jMax = GetMaxRow(jarr);
+
+                for (int i = 0; i < jMax.Length; i++)
+                {
+                    Console.WriteLine("Jagged Max = " + jMax[i]);
+                }
+
+                int[] sum = GetSumJagged(jarr);
+
+                for (int i = 0; i < sum.Length; i++)
+                {
+                    Console.WriteLine("Sum Jagged = " + sum[i]);
+                }
+            }
+
+            static int[][] CreatJagedArrays()
+            {
+                Random rnd = new Random();
+
+                int[][] array = new int[4][];
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    array[i] = new int[8];
+                }
+
+                return array;
+            }
+
+            static int[][] CreatRandomArray(int[][] array)
+            {
+                Random rnd = new Random();
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for (int j = 0; j < array[i].Length; j++)
+                    {
+                        array[i][j] = rnd.Next(1, 100);
+                    }
+                }
+
+                return array;
+            }
+
+            static void Print(int[][] array)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for (int j = 0; j < array[i].Length; j++)
+                    {
+                        Console.Write(array[i][j] + "\t ");
+                    }
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+            static int[] GetMaxRow(int[][] array)
+            {
+                int[] max = new int[array.GetLength(0)];
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    max[i] = array[i][0];
+                    for (int j = 0; j < array[i].Length; j++)
+                    {
+                        if (max[i] < array[i][j])
+                        {
+                            max[i] = array[i][j];
+                        }
+                    }
+                }
+
+                return max;
+            }
+
+            static int[] GetSumJagged(int[]  [] array)
+            {
+                int[] sum = new int[array.Length];
+
+                for (int i = 0; i < array.Length; i++)
+                {
+                    sum[i] = array[i].Sum();
+                }
+
+                return sum;
+            }
+
+        }
+    
+}
--- Homework 28.06/Homework 28.06/Program.cs
+++ Homework 28.06/Homework 28.06/Program.cs
@@ -18,9 +18,15 @@ namespace Homework_28._06
             for (int i = 0; i < max.Length; i++)
             {
                 Console.WriteLine($"max value of each row: {max[i]}");
-
             }
 
+            int[] maxCol = GetMaxColumns(array);
+            for (int i = 0; i < maxCol.Length; i++)
+            {
+                Console.WriteLine($"max value of each column: {maxCol[i]}");
+            }
+
+            Console.ReadLine();
         }
 
         static int[,] CreatRandomArray(int count1, int count2)
@@ -63,10 +69,25 @@ namespace Homework_28._06
                         max[i] = array[i, j];
                     }
                 }
-
-
             }
+            return max;
+        }
 
+        static int[] GetMaxColumns(int[,] array)
+        {
+            int[] max = new int[array.GetLength(1)];
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    max[j] = array[0, j];
+                    if (max[j] < array[i, j])
+                    {
+                        max[j] = array[i, j];
+                    }
+                }
+            }
             return max;
         }
     }
