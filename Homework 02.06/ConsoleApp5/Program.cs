024da2d522ca9610ec7195628d5cf4f9715f9113
ValAs1 <37310121+ValAs1@users.noreply.github.com>
2018-07-10 17:12:17 +0400

    17.12

--- Homework 02.06/ConsoleApp5/Program.cs
+++ Homework 02.06/ConsoleApp5/Program.cs
@@ -9,9 +9,57 @@ namespace ConsoleApp5
     class Program
     {
         //3 unenq zangvac gtnel parz tver@ (list - i mej )
-       
+
         static void Main(string[] args)
         {
+            int[] NewArray = CreateRandomArray(7);
+            PrintArray(NewArray);
+            SelectionSort(7);
+            Console.ReadLine();
+        }
+
+        static int[] CreateRandomArray(int count)
+        {
+            int[] arr = new int[count];
+            Random rnd = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(1, 100);
+            }
+            return arr;
+        }
+
+        static void PrintArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static int[] SelectionSort(int count)
+        {
+            int[] arr = CreateRandomArray(count);
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                int minval = i;  // min value-n stugvox indexn e
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[minval])
+                    {
+                        minval = j;
+                    }
+                }
+                if (minval != i)
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[minval];
+                    arr[minval] = temp;
+                }
+            }
+           // PrintArray(arr);
+            return arr;
         }
     }
 }
