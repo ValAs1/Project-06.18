d92818c2ad864e9aac414868c035248e8386136f
ValAs1 <37310121+ValAs1@users.noreply.github.com>
2018-07-11 07:08:14 +0400

    7;08

---
+++ Homework 10.07/Homework 10.07/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Homework_10._07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           int[] arr = CreatRandomArray(4);
+            PrintArray(arr);
+            
+            Console.ReadLine();
+        }
+        static int[] CreatRandomArray(int count)
+        {
+            int[] array = new int[count];
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(0, 2);
+            }
+            return array;
+        }
+        static void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(" " + array[i]);
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
