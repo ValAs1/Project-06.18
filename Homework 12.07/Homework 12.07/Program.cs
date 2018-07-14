ad88789cf02de05e51faebf14df1b7827a747caa
ValAs1 <37310121+ValAs1@users.noreply.github.com>
2018-07-14 13:34:09 +0400

    12

--- Homework 12.07/Homework 12.07/Program.cs
+++ Homework 12.07/Homework 12.07/Program.cs
@@ -10,53 +10,150 @@ namespace Homework_12._07
     {
         static void Main(string[] args)
         {
-            string[] name = new string[] { "Samvel   ", " Haykuhi", "Anahit", "Hripsime", "Karine  ", "Mery   " };
-            string[] surname = new string[] { "Simonyan", "Atoyan", "Khachatryan", "Babayan", "Karapetyan", "Vardanyan" };
-            string[] fName = new string[] { "Serob   ", "Gagik   ", "Vladilen", "Samvel  ", "Karlen  ", "Chgitem  " };
-            ShafleName(name, surname, fName);
-            PrintArray(name);
-            PrintArray(surname);
-            PrintArray(fName);
+            string[] names = CreateNames(11);
+            string[] surnames = CreateSurNames(11);
+            int[] ages = CreateAges(11);
+            int[] ids = CreateIds(11);
+            int[] shIds = Shuffle(ids);
+            int[][] emptyGroup = EmptyGroup(shIds, 3);
+            int[][] groupedId = GroupedId(shIds, emptyGroup);
+            PrintArray(names, surnames, ages, ids);
+
+            Console.WriteLine("################################################");
+
+            PrintArray(names, surnames, ages, shIds);
+            Console.WriteLine("Grouped");
+            PrintArray(names, surnames, ages, groupedId);
+            Console.WriteLine("################################################");
             Console.Read();
+        }
+        static int[][] GroupedId(int[] shList, int[][] groupList)
+        {
 
-        }
-        static void PrintArray(string[] arr)
-        {
-            foreach (string item in arr)
+            int temp = 0;
+            for (int i = 0; i < groupList.Length; i++)
             {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine();
-        }
-        static void ShufleStingArray(string[] arr)
-        {
-            Random rnd = new Random();
-            for (int i = 0; i < arr.Length; i++)
-            {
-
-                int tempIndex = rnd.Next(0, arr.Length - 1);
-                string tempStr = arr[i];
-                arr[i] = arr[tempIndex];
-                arr[tempIndex] = tempStr;
-            }
-        }
-        static void ShafleName(string[] name, string[] surname, string[] fName)
-        {
-            ShufleStingArray(name);
-            ShufleStingArray(surname);
-            ShufleStingArray(fName);
-        }
-        static string[][] ToJaray(string[] name, string[] surname, string[] fName)
-        {
-            string[][] jarr = new string[name.Length][];
-            for (int i = 0; i < jarr.Length; i++)
-            {
-                for (int j = 0; j < name.Length; j++)
+                for (int j = 0; j < groupList[i].Length; j++)
                 {
-                    jarr[i][j] = name[i];
+
+                    groupList[i][j] = shList[temp];
+                    temp++;
                 }
             }
-            return jarr;
+            return groupList;
+        }
+        static int[][] EmptyGroup(int[] shIds, int num)
+        {
+            int[][] groupeList = new int[num][];
+            for (int i = 0; i < num; i++)
+            {
+                int length = shIds.Length;
+                if (length % num == 0 && length >= num)
+                {
+                    groupeList[i] = new int[length / num];
+                }
+                else if (length % num != 0 && length >= num)
+                {
+                    int c = length - num * (length / num);
+
+                    int b = (length - c) / num;
+                    groupeList[i] = new int[b];
+                    if (i >= num - c)
+                    {
+                        groupeList[i] = new int[b + 1];
+                    }
+                }
+
+            }
+            return groupeList;
+        }
+        static int[] Shuffle(int[] id)
+        {
+            List<int> list = new List<int>(id.Length);
+            List<int> list1 = new List<int>(id.Length);
+            int[] idNew = new int[id.Length];
+            list.AddRange(id);
+            Random rnd = new Random();
+
+            for (int i = 0; i < id.Length; i++)
+            {
+                int item = rnd.Next(0, list.Count - 1);
+                list1.Add(list[item]);
+                list.RemoveAt(item);
+            }
+            idNew = list1.ToArray();
+            return idNew;
+        }
+        static int[] CreateIds(int count)
+        {
+            int[] ids = new int[count];
+            for (int i = 0; i < ids.Length; i++)
+            {
+                ids[i] = i;
+            }
+            return ids;
+        }
+
+        static void PrintArray(string[] arr, string[] arr1, int[] arr2, int[][] jarr)
+        {
+
+            for (int i = 0; i < jarr.Length; i++)
+            {
+                Console.WriteLine(i + 1 + "- GROUP!!!!!!!!!!!!!!!!!!!!!!!!!!1");
+                Console.WriteLine();
+                for (int j = 0; j < jarr[i].Length; j++)
+                {
+                    Console.Write("Name:" + arr[jarr[i][j]] + "  surname: " + arr1[jarr[i][j]] + "  age:" + arr2[jarr[i][j]]);
+                    Console.WriteLine();
+                }
+
+            }
+        }
+
+        static void PrintArray(int[] arr, int[] id)
+        {
+            for (int i = 0; i < id.Length; i++)
+            {
+                Console.Write($"{arr[id[i]]}    ");
+            }
+        }
+        static void PrintArray(string[] arr, string[] arr1, int[] arr2, int[] id)
+        {
+            for (int i = 0; i < id.Length; i++)
+            {
+                Console.Write(arr[id[i]] + " " + arr1[id[i]] + " " + arr2[id[i]] + " ");
+                Console.WriteLine();
+
+            }
+        }
+        static int[] CreateAges(int count = 10, int minValue = 20, int maxValue = 100)
+        {
+            Random rnd = new Random();
+            int[] ages = new int[count];
+
+            for (int i = 0; i < ages.Length; i++)
+            {
+                ages[i] = rnd.Next(minValue, maxValue);
+            }
+            return ages;
+        }
+        static string[] CreateSurNames(int count)
+        {
+            string[] surnames = new string[count];
+            for (int i = 0; i < surnames.Length; i++)
+            {
+                surnames[i] = "A" + i + "yan";
+            }
+            return surnames;
+        }
+        static string[] CreateNames(int count)
+        {
+            string[] names = new string[count];
+            for (int i = 0; i < names.Length; i++)
+            {
+                names[i] = "A" + i;
+            }
+            return names;
         }
     }
 }
