f08053b191eabe6d99996596d1771c9bfe75654d
ValAs <asatryan.volodya@gmail.com>
2018-07-26 22:53:25 +0400

    26.07

---
+++ Homework 26.07/Homework 26.07/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_26._07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kube kube = new Kube()
+            //{
+            //    cou = 6
+            //};
+
+            var kb = new Kube
+            {
+                cou = 22
+            };
+
+            // int cou = 5;
+            Console.ReadLine();
+        }
+    }
+}
